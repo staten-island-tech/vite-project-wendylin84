908ddc2af1b2dc5ccb90ea89f79ff46d03d407f4
wendylin84 <wendyl84@nycstudents.net>
2025-11-13 14:00:21 -0500

    d

---
+++ index.js
@@ -0,0 +1,7 @@
+// make an array of items
+// create (add) the cards on js
+// use css/js to organize & make it look better
+// work on filter buttons
+// event listener with cards (pop up)
+// add header
+// light/dark mode & polish design
