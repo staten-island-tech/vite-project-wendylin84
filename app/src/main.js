befb2dfce890729f347535cdfec9144153309ac9
wendylin84 <wendyl84@nycstudents.net>
2025-11-15 17:11:17 -0500

    Revert "n"
    This reverts commit 0f6c26a78ebb202a89631d49995547cc91322ce4.

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import "./style.css";
+/* import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
@@ -21,4 +21,150 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter')) */
+const items = [
+  {
+    name: "Because of You",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Caught You Boy",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Every Man Gets His Wish",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Trash Magic",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "You Can Be the Boss",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "1949",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Push Me Down",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Wayamaya",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Meet Me in the Pale Moonlight",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "St. Tropez Party Girl",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Jealous Girl",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Prom Song (Gone Wrong)",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Break My Fall",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Resistance",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Heavy Hitter",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "True Love on the Side",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Is It Wrong",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "I Want It All",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Last Girl on Earth",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Put the Radio On",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "Tired of Singing the Blues",
+    img: "",
+    alt: "",
+    category: "",
+  },
+  {
+    name: "",
+    img: "",
+    alt: "",
+    category: "",
+  },
+];
+function inject(item) {
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
+    <h2 class="card-header">${item.name}</h2>
+      <img class="card-img" src="${item.img}"/>
+      <button class="card-info">Know More</button>
+    </div>`
+  );
+}
+item.forEach((item) => inject(item));
